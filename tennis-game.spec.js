5fb3129dce1748b34e530ea5c8294dc9450b00c5
Sontaya58160434 <58160434@go.buu.ac.th>
2017-09-02 22:08:58 +0700

    when Player A scores 2 points and Player B scores 3 points

--- tennis-game.spec.js
+++ tennis-game.spec.js
@@ -5,6 +5,7 @@ function TennisGame() {
 
     this.reset = () => {
         this.playerAScore = 0
+        this.playerBScore = 0
     }
 
     this.echo = () => {
@@ -55,4 +56,21 @@ test('Echo "Love - Fifteen" when playerB get first score', () => {
 
   //Assert
   expect(result).toBe('Love - Fifteen')
+})
+
+test('Echo "Thirty - Forthy" when Player A scores 2 points and Player B scores 3 points', () => {
+  //Arrange
+  let app = new TennisGame
+  app.reset()
+  app.playerAGetScore()
+  app.playerAGetScore()
+  app.playerBGetScore()
+  app.playerBGetScore()
+  app.playerBGetScore()
+
+  //Act
+  let result = app.echo()
+
+  //Assert
+  expect(result).toBe('Thirty - Forthy')
 })
